db4175c9f82c644f5aa670758853326f2dd2067f
Alaa Hossam <160499356+AlaaH0ssam@users.noreply.github.com>
2025-07-18 13:12:55 +0300

    update

---
+++ Flight/Payment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight
+{
+    internal class Payment
+    {
+        public enum PaymentMethod
+        {
+            cash,
+            Instapay,
+            Visa,
+        }
+        public int PaymentID { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public PaymentMethod Paymentmethod { get; set; }
+        public bool PaymentStatus { get; set; }
+
+        public Payment(int id , decimal amount , PaymentMethod method , bool status ) {
+            PaymentAmount = amount;
+            Paymentmethod = method;
+            PaymentStatus = status;
+            PaymentID = id;
+        }
+    }
+}
---
+++ Flight/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight
+{
+    internal class Ticket
+    {
+        public int TicketNumber { get; set; }
+        public decimal Fare { get; set; }
+        public bool IsActive { get; set; }
+        public Booking Booking { get; set; }
+
+        public Ticket(int ticketnum , decimal fare , bool isactive ) { 
+        TicketNumber = ticketnum;
+            Fare = fare;
+            IsActive = isactive;
+        }
+    }
+}
