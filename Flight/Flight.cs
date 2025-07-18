9deb68326662943820e699c31d46044c9896e049
Alaa Hossam <160499356+AlaaH0ssam@users.noreply.github.com>
2025-07-18 12:08:55 +0300

    flight entity

---
+++ Flight/Flight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight
+{
+    internal class flight
+    {
+        public int FlightNumber { get; set; }
+        public string DepartureAirport { get; set; }
+        public string ArrivalAirport { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int AvailableSeats { get; set; }
+
+        public flight(int fnum,string depairport , string arriairport , DateTime deptime , DateTime arrtime , int avseats ) {
+            FlightNumber = fnum;
+            DepartureAirport = depairport;
+            ArrivalAirport = arriairport;
+            DepartureTime = deptime;
+            ArrivalTime = arrtime;
+            AvailableSeats = avseats;
+
+        }
+    }
+}
